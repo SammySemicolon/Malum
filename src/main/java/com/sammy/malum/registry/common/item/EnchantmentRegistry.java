87970ca083ced807757294f24817fbba21e4a3e5
SammySemicolon <malummoddevelopment@gmail.com>
2024-10-25 05:48:24 +0200

    Wawa

--- src/main/java/com/sammy/malum/registry/common/item/EnchantmentRegistry.java
+++ src/main/java/com/sammy/malum/registry/common/item/EnchantmentRegistry.java
@@ -8,6 +8,12 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class EnchantmentRegistry {
 
+    public static final EnchantmentCategory SOUL_HUNTER_WEAPON = EnchantmentCategory.create(MalumMod.MALUM + ":soul_hunter_only", i -> i.getDefaultInstance().is(ItemTagRegistry.SOUL_HUNTER_WEAPON));
+    public static final EnchantmentCategory SCYTHE = EnchantmentCategory.create(MalumMod.MALUM + ":scythe_only", i -> i.getDefaultInstance().is(ItemTagRegistry.SCYTHE));
+    public static final EnchantmentCategory REBOUND_SCYTHE = EnchantmentCategory.create(MalumMod.MALUM + ":rebound_only", i -> i.getDefaultInstance().is(ItemTagRegistry.SCYTHE) || (CommonConfig.ULTIMATE_REBOUND.getConfigValue() && i instanceof TieredItem));
+    public static final EnchantmentCategory STAFF = EnchantmentCategory.create(MalumMod.MALUM + ":staff_only", i -> i.getDefaultInstance().is(ItemTagRegistry.STAFF));
+    public static final EnchantmentCategory SCYTHE_OR_STAFF = EnchantmentCategory.create(MalumMod.MALUM + ":scythe_or_staff", i -> i.getDefaultInstance().is(ItemTagRegistry.SCYTHE) || i.getDefaultInstance().is(ItemTagRegistry.STAFF));
+
     public static final ResourceKey<Enchantment> REBOUND = keyOf("rebound");
     public static final ResourceKey<Enchantment> HAUNTED = keyOf("haunted");
     public static final ResourceKey<Enchantment> SPIRIT_PLUNDER = keyOf("spirit_plunder");
